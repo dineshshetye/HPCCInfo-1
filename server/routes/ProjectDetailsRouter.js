467ed168a69b35358aba93f6f2bf7c69a1419d63
sachin <sachin@empressinfotech.com>
2017-10-24 22:08:42 +0530

    Initial commit

---
+++ server/routes/ProjectDetailsRouter.js
@@ -0,0 +1,68 @@
+//Dependencies
+var express = require('express');
+var router = express.Router();
+var ProjectDetails = require('../models/ProjectDetails');
+const logger = require("../utils/logger");
+
+router.get('/:name?',function(req, res, next) {
+	if(!req.params.name.includes('.')){
+	    ProjectDetails.getProjectByid(req.params.name, function(err, rows) {
+	        if(err) {
+	        	logger.error("REQUEST URL: "+req.url+" , REQUEST IP:  "+ req.ip +", RESPONSE STATUS CODE: " +res.statusCode);
+				logger.error("ERROR MESSAGE: "+ err);
+	        	res.json(err);
+	        } else {
+	            res.json(rows);
+	        }
+	    });
+	} else {
+		ProjectDetails.getProjectByemail(req.params.name, function(err, rows) {
+		    if(err) {
+		    	logger.error("REQUEST URL: "+req.url+" , REQUEST IP:  "+ req.ip +", RESPONSE STATUS CODE: " +res.statusCode);
+				logger.error("ERROR MESSAGE: "+ err);
+		    	res.json(err);
+		    } else {
+		        res.json(rows);
+		    }
+	    });
+	}
+});
+
+router.put('/:name',function(req, res, next) {
+    ProjectDetails.updateProject(req.params.name, req.body, function(err, rows) {
+        if(err) {
+        	logger.error("REQUEST URL: "+req.url+" , REQUEST IP:  "+ req.ip +", RESPONSE STATUS CODE: " +res.statusCode);
+			logger.error("ERROR MESSAGE: "+ err);
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+router.post('/', function(req, res, next){
+	ProjectDetails.addProject(req.body, function(err, count) {
+    	if(err) {
+    		logger.error("REQUEST URL: "+req.url+" , REQUEST IP:  "+ req.ip +", RESPONSE STATUS CODE: " +res.statusCode);
+			logger.error("ERROR MESSAGE: "+ err);
+            res.json(err);
+        } else {
+            res.json(req.body); //TODO: Return count for 1 & 0
+        }
+    });
+});
+
+router.delete('/:name',function(req, res, next){
+	ProjectDetails.deleteProject(req.params.name, function(err, count) {
+	    if(err) {
+	    	logger.error("REQUEST URL: "+req.url+" , REQUEST IP:  "+ req.ip +", RESPONSE STATUS CODE: " +res.statusCode);
+			logger.error("ERROR MESSAGE: "+ err);
+	        res.json(err);
+	    } else {
+	        res.json(count);
+	    }
+    });
+});
+
+//Return router
+module.exports = router;
---
+++ server/scripts/HPCCInfo_schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE  IF NOT EXISTS `hpccinfo`;
+USE `hpccinfo`;
+
+--
+-- Table structure for table `ProjectDetails`
+--
+
+CREATE TABLE `projectdetails` (
+  `projectdata` TEXT NOT NULL,
+  `projectid` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `updatedby` varchar(50) NOT NULL,
+  `dateupdated` timestamp,
+  `file` varchar(100)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
