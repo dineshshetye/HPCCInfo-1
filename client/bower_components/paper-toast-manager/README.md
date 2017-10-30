3efe2b5c09d05f3c4f5af265e32904f65ede6892
sachin <sachin@empressinfotech.com>
2017-10-30 20:41:23 +0530

    New, edit and Open Workbook
    Plugin dropdown is having issues.

---
+++ client/bower_components/paper-toast-manager/README.md
@@ -0,0 +1,18 @@
+# paper-toast-manager
+
+An easier way to generate paper-toasts at runtime.
+
+Example:
+
+    <paper-toast-manager></paper-toast-manager>
+    paperToastManager.toast('Hello, world!');
+
+## Installation
+
+Install with:
+
+    bower install --save paper-toast-manager
+
+If you don't have [Bower](http://bower.io/) yet, you can install that via:
+
+    npm install -g bower
---
+++ client/node_modules/vaadin-grid/wct.conf.js
@@ -0,0 +1,67 @@
+var argv = require('yargs').argv;
+
+module.exports = {
+  testTimeout: 180 * 1000,
+  registerHooks: function(context) {
+    var saucelabsPlatforms = [
+      // TODO: Excluded iOS tests from the hybrid version. Re-enable for P2 version.
+      // 'OS X 10.12/iphone@10.2',
+      // 'OS X 10.12/ipad@10.2',
+      'Windows 10/microsoftedge@14',
+      'Windows 10/internet explorer@11',
+      'OS X 10.12/safari@10.0'
+    ];
+
+    var cronPlatforms = [
+      'Windows 10/chrome@55',
+      'Windows 10/firefox@50'
+    ];
+
+    if (argv.env === 'saucelabs') {
+      context.options.plugins.sauce.browsers = saucelabsPlatforms.concat(cronPlatforms);
+
+    } else if (argv.env === 'saucelabs-cron') {
+      context.options.plugins.sauce.browsers = cronPlatforms;
+
+    // Add coverage for local tests only
+    } else {
+      context.options.plugins.istanbul = {
+        'dir': './coverage',
+        'reporters': ['text-summary', 'lcov'],
+        'include': [
+          '/iron-list-behavior.html',
+          '/vaadin-grid-active-item-behavior.html',
+          '/vaadin-grid-array-data-provider-behavior.html',
+          '/vaadin-grid-cell-click-behavior.html',
+          '/vaadin-grid-column.html',
+          '/vaadin-grid-data-provider-behavior.html',
+          '/vaadin-grid-dynamic-columns-behavior.html',
+          // TODO: @limonte, revisit this in future, currently a weird istanbul bug here
+          // '/vaadin-grid-filter-behavior.html',
+          '/vaadin-grid-focusable-cell-container-behavior.html',
+          '/vaadin-grid-keyboard-navigation-behavior.html',
+          '/vaadin-grid-row-details-behavior.html',
+          '/vaadin-grid-selection-behavior.html',
+          '/vaadin-grid-selection-column.html',
+          '/vaadin-grid-sizer.html',
+          // TODO: @limonte, revisit this in future, currently a weird istanbul bug here
+          // '/vaadin-grid-sort-behavior.html',
+          '/vaadin-grid-table-cell.html',
+          '/vaadin-grid-table-focus-trap.html',
+          '/vaadin-grid-table-header-footer.html',
+          '/vaadin-grid-table-outer-scroller.html',
+          '/vaadin-grid-table-row.html',
+          '/vaadin-grid-table-scroll-behavior.html',
+          '/vaadin-grid-table.html',
+          '/vaadin-grid-templatizer.html',
+          '/vaadin-grid.html'
+        ],
+        'exclude': []
+      };
+    }
+  },
+
+  plugins: {
+    'random-output': true
+  }
+};
